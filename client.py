fd24bfcfb501e0ae4804b613abd8891f21b6dc7b
Joe <joseph.reeve@googlemail.com>
2014-12-06 15:45:39 +0000

    Added MAC

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
-import subprocess
-import json
-import urllib2
+import subprocess, json, urllib2, fcntl, socket, struct
+
+def getHwAddr(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+    return ':'.join(['%02x' % ord(char) for char in info[18:24]])
+
 
 data = {
         'boots': len(subprocess.check_output(['last','reboot']).split(b'\n')) -3,
         'uptime': subprocess.check_output('uptime'),
         'cid': open('/sys/block/mmcblk0/device/cid').read().rstrip()
+        'MAC': getHwAddr('wlan0')
         }
 
 req = urllib2.Request('http://atslash.com:4000/')
