700e2c1cd0b8ab3c9076a241e4646b9cec861fa0
Joe <joseph.reeve@googlemail.com>
2014-12-06 15:48:49 +0000

    Removed extra return character

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ def getHwAddr(ifname):
 
 data = {
         'boots': len(subprocess.check_output(['last','reboot']).split(b'\n')) -3,
-        'uptime': subprocess.check_output('uptime'),
+        'uptime': subprocess.check_output('uptime').rstrip(),
         'cid': open('/sys/block/mmcblk0/device/cid').read().rstrip(),
         'MAC': getHwAddr('wlan0')
         }
