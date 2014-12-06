795e3a06fe22484c55f7302c590098a8b9dbc949
Joe <joseph.reeve@googlemail.com>
2014-12-06 15:29:53 +0000

    Reverted to URLLib

--- client.py
+++ client.py
@@ -1,14 +1,13 @@
 import subprocess
 import json
-import requests
 
 data = {
         'boots': len(subprocess.check_output(['last','reboot']).split(b'\n')) -3,
-	'uptime': subprocess.check_output('uptime'),
-	'cid': open('/sys/block/mmcblk0/device/cid').read().rstrip()
-}
+        'uptime': subprocess.check_output('uptime'),
+        'cid': open('/sys/block/mmcblk0/device/cid').read().rstrip()
+        }
 
-url = 'http://atslash.com:4000/'
-headers = {'content-type': 'application/json'}
+req = urllib2.Request('http://atslash.com:4000/')
+req.add_header('Content-Type', 'application/json')
 
-r = requests.post(url, data=json.dumps(data), headers=headers)
+response = urllib2.urlopen(req, json.dumps(data))
