900e82bb1ebf9901a00c430ebf1616c3c3148828
Joe <joseph.reeve@googlemail.com>
2014-12-06 14:48:04 +0000

    Starting blocks of JSON post

--- client.py
+++ client.py
@@ -1,2 +1,11 @@
 import subprocess
-print(len(subprocess.check_output(['last','reboot']).split(b'\n')) -3) #Gets number of boots
+import json
+
+data = {
+        'boots': len(subprocess.check_output(['last','reboot']).split(b'\n')) -3
+}
+
+req = urllib2.Request('http://abc.com/api/posts/create')
+req.add_header('Content-Type', 'application/json')
+
+response = urllib2.urlopen(req, json.dumps(data))
