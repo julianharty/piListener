7ebdf691eb77e3febd6d5c58f201df277d3e1a30
Joe <joseph.reeve@googlemail.com>
2014-12-06 14:54:46 +0000

    Added More data

--- client.py
+++ client.py
@@ -2,10 +2,12 @@ import subprocess
 import json
 
 data = {
-        'boots': len(subprocess.check_output(['last','reboot']).split(b'\n')) -3
+        'boots': len(subprocess.check_output(['last','reboot']).split(b'\n')) -3,
+	'uptime': subprocess.check_output('uptime'),
+	'cid': open('/sys/block/mmcblock0/device/cid').read().rstrip()
 }
 
-req = urllib2.Request('http://abc.com/api/posts/create')
+req = urllib2.Request('http://atslash.com:1337/')
 req.add_header('Content-Type', 'application/json')
 
 response = urllib2.urlopen(req, json.dumps(data))
