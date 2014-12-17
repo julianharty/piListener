2971a52b657edc16a79699cb4778e1ef33a1ec3b
Joe <joseph.reeve@googlemail.com>
2014-12-17 11:39:09 +0000

    Tries eth0 then wlan0

--- client.py
+++ client.py
@@ -1,8 +1,12 @@
 import subprocess, json, fcntl, socket, struct, sys, urllib2
 
-def getHwAddr(ifname):
+def getHwAddr():
+    ifname=['eth0','wlan0']
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[:15]))
+    try: 
+	info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[0][:15]))
+    except: 
+	info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', ifname[1][:15]))
     return ':'.join(['%02x' % ord(char) for char in info[18:24]])
 
 
@@ -10,7 +14,7 @@ data = {
         'boots': len(subprocess.check_output(['last','reboot']).split(b'\n')) -3,
         'uptime': subprocess.check_output('uptime').rstrip(),
         'cid': open('/sys/block/mmcblk0/device/cid').read().rstrip(),
-        'MAC': getHwAddr('wlan0'),
+        'MAC': getHwAddr(),
         'isBoot': True if len(sys.argv)>=3 else False
         }
 
